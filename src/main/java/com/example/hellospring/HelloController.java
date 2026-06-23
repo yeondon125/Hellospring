509b92492ae06a97df8e5deb4b63691de60ca569
yeondon <s25030@gsm.hs.kr>
2026-06-23 18:02:42 +0900

    PathVariable 변경

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hellospring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class HelloController {
     @GetMapping("/hello-json")
     public HelloResponse helloJson(@RequestParam(defaultValue = "world") String name) {
         return new HelloResponse(name, "Hello, " + name + "!");
+    }
+
+    @GetMapping("/members/{id}")
+    public String getMember(@PathVariable Long id) {
+        return "회원 ID : " + id;
     }
 }
