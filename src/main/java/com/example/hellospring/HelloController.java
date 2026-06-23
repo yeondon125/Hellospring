d22ca8389b334d059c377b10714a7d351605afc8
yeondon <s25030@gsm.hs.kr>
2026-06-23 11:48:56 +0900

    get 메서드

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -1,6 +1,15 @@
 package com.example.hellospring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    public static void main(String[] args) {
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "world") String name) {
+        return "Hello, " + name + "!";
     }
 }
